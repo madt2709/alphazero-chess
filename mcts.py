e4ed3affa98cc7de95a385cc9bea7dd7ed08e712
Theo Dias <theodore.dias@hokodo.co>
2022-12-21 22:36:01 +0100

    Added self play for one game

--- mcts.py
+++ mcts.py
@@ -1,9 +1,11 @@
 import chess
 import numpy as np
 import math
+import collections
 
 from representations.moves import decode_move, encode_actions
 from representations.board import encode_board, decode_board
+from settings import NUM_OF_MCTS_SEARCHES
 
 
 class UCTNode():
@@ -21,11 +23,11 @@ class UCTNode():
         self.s = state
         self.move = move
         self.parent = parent
-        self.children = {}
+        self.children = {}  # key = idx_of_action required to reach child node, value = child node
         self.child_number_of_visits = np.zeros([64, 73]).astype(int)
         self.child_total_value = np.zeros([64, 73]).astype(int)
-        self.action_idxs = []  # store the idxs of legal actions of given state
-        # TO DO set policy to current neural net best estimation
+        self.legal_action_idxs = []  # store the idxs of legal actions of given state
+        # TO DO add the legal action idxs properly
 
     @property
     def number_of_visits(self):
@@ -44,28 +46,33 @@ class UCTNode():
         self.parent.child_total_value[self.move] = value
 
     def child_Q(self):
-        return (self.child_total_value * self.child_number_of_visits + self.total_value) / (self.child_number_of_visits + 1)
+        return self.child_total_value / (self.child_number_of_visits + 1)
 
     def child_U(self, exploration_rate):
-        return self.child_Q + exploration_rate*self.policy*math.sqrt(sum([sum(i) for i in self.child_number_of_visits]))/(1+self.child_number_of_visits)
+        return exploration_rate*self.policy*math.sqrt(sum([sum(i) for i in self.child_number_of_visits]))/(1+self.child_number_of_visits)
 
     def best_child(self):
         if self.action_idxs != []:
-            child_U = self.child_U
+            func_to_max = self.child_U + self.child_Q
             max_value = -10000
             max_idx = (-1, -1)
             for i in self.action_idxs:
-                if max_value < child_U[i]:
-                    max_value = child_U[i]
+                if max_value < func_to_max[i]:
+                    max_value = func_to_max[i]
                     max_idx = i
         return i
 
-    def check_if_child_node_exists(self, state):
-        return state in self.children.keys()
+    def check_if_child_node_exists(self, action_idx):
+        return action_idx in self.children.keys()
 
     def search(self, nnet):
         """
-        Method to perform MCTS search on a given node. Note we return the negative value becaue we expect the next search to be from perspective of other player.
+        Method to perform MCTS search on a given node. 
+        Inputs:
+            -nnet: neural net used to evaluate position
+        Outputs:
+            - total_value: value determined by MCTS algo
+        Note we return the negative value becaue we expect the next search to be from perspective of other player.
         """
         # check if game has ended
         board = decode_board(self.s)
@@ -77,7 +84,7 @@ class UCTNode():
                 self.total_value = 0
             else:
                 self.total_value = -1
-            return -self.total_value
+            return -self.total_value, self
 
         # find action a which maximises U
         best_action = self.best_child()
@@ -90,11 +97,24 @@ class UCTNode():
             self.child_number_of_visits[best_action] += 1
             # predict
             p_s, self.child_total_value[best_action] = nnet.predict(next_s)
-            return -self.child_total_value[best_action]
+            return -self.child_total_value[best_action], self.children[best_action]
         else:
             child_v = self.children[best_action].search()
             self.child_number_of_visits[best_action] += 1
             return self.children[best_action].search()
+
+    def backpropogate(self, value):
+        current = self
+        # use this to track the colour of the player's value being updated.
+        counter = True
+        while current is not None:
+            current.parent.child_number_of_visits[self.move] += 1
+            if counter:
+                current.parent.total_value[self.move] += (1*value)
+            else:
+                current.parent.total_value[self.move] += (-1*value)
+            current = current.parent
+            counter = not counter
 
 
 def get_next_state(s, action_idx):
@@ -105,3 +125,55 @@ def get_next_state(s, action_idx):
     # encode next board
     next_state = encode_board(board)
     return next_state
+
+
+class DummyNode():
+    """
+    A dummy class used when initalising an MCTS iteration
+    """
+
+    def __init__(self):
+        self.parent = None
+        self.child_total_value = collections.defaultdict(float)
+        self.child_number_of_visits = collections.defaultdict(float)
+
+
+def complete_one_mcts(num_of_searches, nnet, starting_position=chess.Board()):
+    root = UCTNode(encode_board(starting_position),
+                   move=None, parent=DummyNode())
+    for i in range(num_of_searches):
+        value = root.search(nnet)
+        root.backpropogate(value)
+    return np.argmax(root.child_number_of_visits), root
+
+
+def get_policy(node):
+    """
+    Function to get policy of a node. Should really only be called on root of searches
+    """
+    policy = np.zeros(64, 73)
+    for idx, value in np.ndenumerate(node.child_number_of_visits):
+        policy[idx] = value / sum([sum(i)
+                                  for i in node.child_number_of_visits])
+    return policy
+
+
+def self_play_one_game(num_of_search_iters, nnet, starting_position=chess.Board()):
+    """
+    A function to play 1 training game.
+    Inputs:
+        - num_of-search_iters: this is used to know how many iterations the MCTS algo should perform before picking the best move
+        - nnet: neural net used to evaluate a position
+        - starting_position: the starting position of the training games
+    Outputs: 
+        - a list where each entry is [s,p,v] for each of the states, policy and values encountered in the training game. 
+    eg [[s_1,p_1,v_1],...]
+    """
+    dataset = []  # to add [s,p, v] encountered
+    board = starting_position.copy()
+    while not board.outcome:
+        best_move, root = complete_one_mcts(NUM_OF_MCTS_SEARCHES, nnet, board)
+        policy = get_policy(root)
+        dataset.append([root.s, policy, root.total_value])
+        board.push(decode_move(best_move))
+    return dataset
