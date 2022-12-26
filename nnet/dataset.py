1477ffcddd88d593c12f15163a4a3e63b9e6ca38
Theo Dias <theodore.dias@hokodo.co>
2022-12-26 21:46:22 +0100

    Got rid of warnings

--- nnet/dataset.py
+++ nnet/dataset.py
@@ -6,9 +6,13 @@ class BoardData(Dataset):
     def __init__(self, dataset):
         # dataset is a list of lists of form [s, p, v].
         super().__init__()
-        dataset = np.array(dataset)
-        self.X = dataset[:, 0]
-        self.Y_p, self.Y_v = dataset[:, 1], dataset[:, 2]
+        self.X = []
+        self.Y_p, self.Y_v = [], []
+        for data in dataset:
+            s, p, v = data
+            self.X.append(s)
+            self.Y_p.append(p)
+            self.Y_v.append(v)
 
     def __getitem__(self, idx):
         return self.X[idx], self.Y_p[idx], self.Y_v[idx]
--- nnet/loss.py
+++ nnet/loss.py
@@ -8,6 +8,6 @@ class AlphaLoss(nn.Module):
         self.cross = nn.CrossEntropyLoss(reduction='sum')
 
     def forward(self, y_p, policy, y_v, value):
-        value_error = self.mse(y_v, value)
-        policy_error = self.cross(y_p, policy)
+        value_error = self.mse(y_v.view(-1), value.view(-1))
+        policy_error = self.cross(y_p.view(-1), policy.view(-1))
         return policy_error + value_error
